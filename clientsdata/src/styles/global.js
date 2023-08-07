71e85c8d6e2ef48d8b75cc1c15fde9aae5ffdd71
Ernandes Bitencourt <ernandes.f.bitencourt@gmail.com>
2023-08-07 14:37:29 -0400

    ok

--- clientsdata/src/styles/global.js
+++ clientsdata/src/styles/global.js
@@ -6,9 +6,12 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: ${props => props.theme.Themers.backgroundColor};
     
   }
-  
+  body{
+    background-color: ${props => props.theme.Themers.backgroundColor};
+  }
 
 `;
 
